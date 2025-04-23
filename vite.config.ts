ae821f6f5139d422944d218eb68ee3211c715ddb
Morgan <143568735+Morclouds@users.noreply.github.com>
2025-04-22 20:40:36 -0500

    yeah

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/forestvibe/',  // 👈 Add this line exactly as shown
+  base: '/ForestVibe/',  // 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
